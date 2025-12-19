70aa5e2574a6c36606fdde1cf430d647aec3f506
Akshit Verma <vermaakshit2002march@gmail.com>
2025-12-19 23:03:35 +0530

    newcommit

--- src/Components/QRCodeGenerator.jsx
+++ src/Components/QRCodeGenerator.jsx
@@ -1,64 +1,67 @@
-import { React, useState } from 'react';
+import { useState } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 import './style/style.css';
 
 const QRCodeGenerator = () => {
-    
+
   const [qrValue, setQrValue] = useState('');
   let web;
 
-  const handleClick = () => {
-    let input=document.getElementById("website");
-    let qrcode = document.getElementById("qrcode");
-    if(input.value!==""){
-    web=input.value;
-    qrcode.style.display="inline";
-    setQrValue(web);
+  const handleChange = (e) => {
+    let input = document.getElementById("website");
+    if (!input.value.includes("https://www.") && input.value !== "") {
+      setTimeout(() => {
+        alert("Input is not valid. It must contains https://www.");
+      }, 200);
+      document.getElementsByClassName("generate")[0].style.cursor = "not-allowed";
+      document.getElementsByClassName("generate")[0].disabled = true;
     }
-    else{
+    else {
+      document.getElementsByClassName("generate")[0].style.cursor = "pointer";
+      document.getElementsByClassName("generate")[0].disabled = false;
+    }
+  }
+
+  const handleClick = () => {
+    let input = document.getElementById("website");
+    let qrcode = document.getElementById("qrcode");
+    let p = document.getElementById("paragraph");
+    if (input.value !== "") {
+      web = input.value;
+      p.style.display = "block";
+      qrcode.style.display = "inline";
+      setQrValue(web);
+    }
+    else {
       setQrValue("");
-      qrcode.style.display="none";
+      p.style.display = "none";
+      qrcode.style.display = "none";
       alert("Input value is Empty ! Please fill This");
     }
   }
 
-  const handleChange = (e) => {
-     let input=document.getElementById("website");
-     if(!input.value.includes("https://www.") && input.value!=="")
-     {
-      setTimeout(() => {
-        alert("Input is not valid. It must contains https://www.");
-      }, 200);
-      document.getElementsByClassName("generate")[0].style.cursor="not-allowed";
-      document.getElementsByClassName("generate")[0].disabled=true;
-     }
-     else{
-        document.getElementsByClassName("generate")[0].style.cursor="pointer";
-       document.getElementsByClassName("generate")[0].disabled=false;
-     }
-  }
 
-    return (
+  return (
     <div className="div">
       <h1>QR Code Generator for Any Website</h1>
-      <span style={{display: "flex", justifyContent: "center", gap: "40px"}}>
-        <input type='text' placeholder='Enter any website Name :' id="website" onChange={handleChange}></input>
+      <span className="website">
+        <input type='text' placeholder='Enter any website Name' id="website" onChange={handleChange}></input>
         <button className="generate" onClick={handleClick}>Generate QR Code</button>
-        </span>
+      </span>
       <div>
-      <QRCodeCanvas
-      id={"qrcode"}
-        value={qrValue}
-        size={256}
-        bgColor={"#ffffff"}
-        fgColor={"#000000"}
-        level={"H"}
-        includeMargin={true}
-      />
+        <p id="paragraph">Scan this QR to visit: <br /><strong>{qrValue}</strong></p>
+        <QRCodeCanvas
+          id={"qrcode"}
+          value={qrValue}
+          size={256}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"H"}
+          includeMargin={true}
+        />
       </div>
-      <p>Scan this QR to visit: <br /><strong>{qrValue}</strong></p>            
     </div>
-    )
+  )
 }
 
 export default QRCodeGenerator;
